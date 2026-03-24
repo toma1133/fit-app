e0ac3404ddfff5bbc7fc5bcdcb9cd47b2cb92b57
Nick Su <nick.su@asmpt.com>
2026-03-24 16:52:35 +0800

    Added react-is and pack config

--- vite.config.ts
+++ vite.config.ts
@@ -20,4 +20,24 @@ export default defineConfig({
 			usePolling: true,
 		},
 	},
+	build: {
+		chunkSizeWarningLimit: 1000,
+		rollupOptions: {
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						if (id.includes('firebase')) {
+							return 'firebase';
+						}
+						if (id.includes('recharts')) {
+							return 'charts';
+						}
+						if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
+							return 'vendor';
+						}
+					}
+				}
+			}
+		}
+	}
 });
