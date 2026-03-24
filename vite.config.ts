04dfd3b0c974db92163f43b9da58a91f2fe51d7a
Nick Su <nick.su@asmpt.com>
2026-03-24 17:09:31 +0800

    Update pwa

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from 'path';
 import tailwindcss from '@tailwindcss/vite';
 import react, { reactCompilerPreset } from '@vitejs/plugin-react';
 import babel from '@rolldown/plugin-babel';
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -13,7 +14,31 @@ export default defineConfig({
 			'~': path.resolve(__dirname, 'src'),
 		},
 	},
-	plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss()],
+	plugins: [react(), babel({ presets: [reactCompilerPreset()] }), tailwindcss(), VitePWA({
+		registerType: 'autoUpdate',
+		includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'mask-icon.svg'],
+		manifest: {
+			name: 'FitPro 體態追蹤器',
+			short_name: 'FitPro',
+			description: '您的個人體態與飲食追蹤器',
+			theme_color: '#ffffff',
+			background_color: '#ffffff',
+			display: 'standalone',
+			icons: [
+				{
+					src: 'pwa-192x192.png',
+					sizes: '192x192',
+					type: 'image/png'
+				},
+				{
+					src: 'pwa-512x512.png',
+					sizes: '512x512',
+					type: 'image/png',
+					purpose: 'any maskable'
+				}
+			]
+		}
+	})],
 	server: {
 		host: `0.0.0.0`,
 		watch: {
@@ -39,5 +64,5 @@ export default defineConfig({
 				}
 			}
 		}
-	}
+	},
 });
